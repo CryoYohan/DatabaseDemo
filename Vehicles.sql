ebcf3ecaa19932a7f22804eaa102814c026727d3
Cyril John T. Ypil <cyrilypil@gmail.com>
2024-04-03 14:56:34 +0800

    Add files via upload
    LAB DBS

---
+++ Vehicles.sql
@@ -0,0 +1,9 @@
+﻿CREATE DATABASE Vehicles
+
+use Vehicles
+CREATE TABLE VehiclesTable(
+	v_id int primary key identity not null,
+	PlateNo varchar(50),
+	VehicleType varchar(50),
+	VehicleBrand varchar(50)
+	);
